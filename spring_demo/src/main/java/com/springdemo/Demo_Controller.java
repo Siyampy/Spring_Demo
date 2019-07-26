4a74d1b870a63cfb7e6d07e374c9e2a3d567d216
Siyam <dhanush0206@gmail.com>
2019-07-26 10:33:37 +0530

    @RequestParam() annotations

--- spring_demo/src/main/java/com/springdemo/Demo_Controller.java
+++ spring_demo/src/main/java/com/springdemo/Demo_Controller.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -17,11 +18,11 @@ public class Demo_Controller
 	}
 //	Here we can also use the Annotation based Configuration instead of the xml
 	@RequestMapping("/add")
-	public ModelAndView add(HttpServletRequest rq,HttpServletResponse rs)
+	public ModelAndView add(@RequestParam("s1") String n,HttpServletRequest rq,HttpServletResponse rs)
 	{
-		String n=rq.getParameter("s1").toString();
+//		String n=rq.getParameter("s1").toString();
 		ModelAndView mv=new ModelAndView();
-		mv.setViewName("display.jsp");
+		mv.setViewName("display");
 		mv.addObject("result",n);
 		return mv;
 	}
